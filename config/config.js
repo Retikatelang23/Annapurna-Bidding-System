1d52edbede5fbce145ff36a35431f2f17063db78
Ritika <retikatelang23@gmail.com>
2024-04-27 00:00:52 +0530

    Final Running Bidding Commit

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ const config = {
   jwtSecret: process.env.JWT_SECRET || "abc123",
   mongoUri: process.env.MONGODB_URI ||
     process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
+    'mongodb://' + (process.env.IP || '127.0.0.1') + ':' +
     (process.env.MONGO_PORT || '27017') +
     '/mernproject',
   stripe_connect_test_client_id: 'wefefwqqwf',
